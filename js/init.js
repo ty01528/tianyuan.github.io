6104f3d57435442638458d2eb4598dcbbdeda42f
ty01528 <tianyuan01528@foxmail.com>
2021-12-06 02:09:16 +0800

    2222

--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
+---
+---
 /*
 	Read Only by HTML5 UP
 	html5up.net | @n33co
@@ -8,12 +10,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global: { href: '/tianyuan.github.io/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
-			xlarge: { media: '(max-width: 1680px)', href: '/tianyuan.github.io/css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: '/tianyuan.github.io/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 1024px)', href: '/tianyuan.github.io/css/style-medium.css', containers: '85%!' },
-			small: { media: '(max-width: 736px)', href: '/tianyuan.github.io/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
-			xsmall: { media: '(max-width: 480px)', href: '/tianyuan.github.io/css/style-xsmall.css' }
+			global: { href: '{{ site.baseurl }}/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
+			xlarge: { media: '(max-width: 1680px)', href: '{{ site.baseurl }}/css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: '{{ site.baseurl }}/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			medium: { media: '(max-width: 1024px)', href: '{{ site.baseurl }}/css/style-medium.css', containers: '85%!' },
+			small: { media: '(max-width: 736px)', href: '{{ site.baseurl }}/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
+			xsmall: { media: '(max-width: 480px)', href: '{{ site.baseurl }}/css/style-xsmall.css' }
 		},
 		plugins: {
 			layers: {
